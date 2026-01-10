c9e1eea86f50d3e870bf2149094bc6cd0610fb96
Neely Hopkins <nmhops@icloud.com>
2026-01-10 11:52:26 -0600

    fixed paths so js can run from github pages

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch('../components/footer.html')
+  fetch('components/footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer').innerHTML = data;
--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch('../components/navbar.html')
+  fetch('components/navbar.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('navbar').innerHTML = data;
